7ff2a2545ce36cb8532cdba83df8befcd1e7a2b8
Deepesh Thapa <frm_nep@hotmail.com>
2016-06-06 01:25:11 +0100

    Delete welcome.php

--- lookatme/welcome.php
+++
@@ -1,3 +0,0 @@
-<!-- replace the content with your welcome text -->
-
-This Wordpress theme is compatible with latest WordPress versions, widgets ready, SEO optimized, ready for threaded comments, and tested on various major web browsers: IE, FF, Safari.
