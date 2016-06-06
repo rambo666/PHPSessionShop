8aacc826b9a04e224da751ec68b3ce066ee76ac6
Deepesh Thapa <frm_nep@hotmail.com>
2016-06-06 01:27:30 +0100

    Delete searchform.php

--- lookatme/searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" action="<?php bloginfo('url'); ?>">
-					<input name="s" type="text" class="searchtext" id="s" value="Search..." onblur="if (this.value == '') {this.value = 'Search...';}" onfocus="if (this.value == 'Search...') {this.value = '';}" />
-					<input type="image" src="<?php bloginfo('stylesheet_directory'); ?>/images/spacer.gif" id="searchsubmit" alt="Search" value="" />
-				</form>
